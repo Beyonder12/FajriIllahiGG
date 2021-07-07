4687d618bce162219684ca3fcd89fc237e0f96ac
Fajr <72368235+Beyonder12@users.noreply.github.com>
2021-07-07 17:33:13 +0700

    Update account.js

--- common/models/account.js
+++ common/models/account.js
@@ -137,6 +137,145 @@ module.exports = function (Account) {
         http: {verb: "post"}
       }
   );
+  
+  
+    Rumahsakit.remoteMethod('getStaffListIsOpenedLedger', {
+    description: ["get staff list"],
+    accepts: [
+      { arg: 'filter', type: 'object', required:true, description:"this is filter of staff" },
+      { arg: 'options', type: 'object', http:"optionsFromRequest"},
+
+    ],
+    returns: {
+      arg: "status", type: "object", root: true, description: "Return value"
+    },
+    http: { verb: "get" }
+  });
+
+  Rumahsakit.getStaffListIsOpenedLedgerV2 = async function (filter, skip, limit, sort, options) {
+    // DESC PO barang
+    // payload : {placeId: "string", search: "string", dateStart: "string", dateEnd: "start"}
+    const { Ledger } = Rumahsakit.app.models;
+
+    try {
+      const token = options && options.accessToken;
+      if(!token) {
+        const error = new Error('Please login to access this feature');
+        error.statusCode = 401;
+        throw error;
+      }
+      const userId = token && token.userId;
+      if(!userId) {
+        const error = new Error('Where do you get this authentication');
+        error.statusCode = 401;
+        throw error;
+      }
+      var constraints = {
+        hospitalId: {presence: true}
+      };
+
+      var validation = validate(filter, constraints);
+      if (validation) {
+        const error = new Error("error");
+        error.statusCode = 412;
+        error.message = validation;
+        throw error;
+      }
+
+
+      var $$QUERY = {}, $$SEARCH_QUERY = {};
+
+
+
+      if (filter.hasOwnProperty('search')) {
+        $$SEARCH_QUERY['$or'] = [
+          {code: {$regex: filter['search'], $options: 'i'}},
+          {hospitalName: {$regex: filter['search'], $options: 'i'}},
+          {status: {$regex: filter['search'], $options: 'i'}},
+        ];
+      }
+
+
+      var $$ITEMS = 1;
+      if (limit != null) {
+        if (skip != null) {
+          $$ITEMS = {$slice: ["$items", skip, limit]};
+        } else {
+          $$ITEMS = {$slice: ["$items", limit]};
+        }
+      }
+
+
+      if (filter.hasOwnProperty('dateStart')) {
+        if (filter.hasOwnProperty('dateEnd')) {
+          $$SEARCH_QUERY['createdAt'] = {$gte: new Date(filter['dateStart']), $lte: new Date(filter['dateEnd'])};
+        } else {
+          $$SEARCH_QUERY['createdAt'] = {$gte: new Date(filter['dateStart'])};
+        }
+      } else {
+        if (filter.hasOwnProperty('dateEnd')) {
+          $$SEARCH_QUERY['createdAt'] = {$lte: new Date(filter['dateEnd'])};
+        }
+      }
+
+
+      var $$LOOKUP_ITEMS = { $lookup: {
+        from: "Masyarakat",
+        localField: "createdId",
+        foreignField: "_id",
+        as: "Creator"
+      }};
+
+      const { connector, ObjectID } = Ledger.getDataSource();
+      const $$hospitalId = ObjectID(filter['hospitalId']);
+      $$QUERY['hospitalId'] = $$hospitalId;
+
+      var $$SORT = {};
+
+      var $$AGGREGATE = [];
+      $$AGGREGATE.push({$match: $$QUERY});
+      // $$AGGREGATE.push({
+      //   $project: {
+      //     _id: 1,
+      //     returnedDate: 1,
+      //     returnCode: 1,
+      //     picReturnClientName: 1,
+      //     hospitalName: 1,
+      //     receivedDate: 1,
+      //     status: 1,
+      //     isActive:1,
+      //   }
+      // });
+      if (filter.hasOwnProperty('search') || filter.hasOwnProperty('dateStart')) $$AGGREGATE.push({$match: $$SEARCH_QUERY});
+      $$AGGREGATE.push($$LOOKUP_ITEMS);
+      $$AGGREGATE.push({$sort: {receivedDate: -1}});
+      $$AGGREGATE.push({$group: { _id: null, count: { $sum:1 }, items: { $push: '$$ROOT'}}});
+      $$AGGREGATE.push({$project: {
+        _id: 0,
+        status: "success",
+        count: 1,
+        items: $$ITEMS
+      }});
+
+      // Raw query
+      const itemCollection = connector.collection('Ledger');
+      const itemCursor = await itemCollection.aggregate($$AGGREGATE);
+      var results = await itemCursor.toArray();
+
+      if (results.length == 0) {
+        results = {status: "success", items: [], count:0 };
+      } else {
+        results = results[0];
+      }
+
+      // let results2 = await ReturnRequestItem.find();
+      // console.log('result2', results2)
+
+      return Promise.resolve(results);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  };
 
 
   Account.disableRemoteMethod("create", true);
